5b087aa02a847e06bc2894d0ca1336d7f4a05077
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-02-11 17:52:57 +0530

    Update 3_aws_cli.sh

--- 3_aws_cli.sh
+++ 3_aws_cli.sh
@@ -5,4 +5,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+mkdir project
+cd project
+echo " Project Folder Created! now Configure AWS CLI "
 aws configure
+
+echo " Create User in AWS, and get the AWS Access and Secret Key"
